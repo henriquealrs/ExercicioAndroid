5c4dadecd88567995555a92b3a004fcf9843b75c
Henrique Silva <henriquealrs@gmail.com>
2019-04-04 11:06:57 -0300

    Aula 10

---
+++ app/src/main/java/com/henriquealrs/android/semana3_aula3/ChronometerViewModel.kt
@@ -0,0 +1,9 @@
+package com.henriquealrs.android.semana3_aula3
+
+import androidx.lifecycle.ViewModel
+
+class ChronometerViewModel : ViewModel() {
+
+    var startTime = 0L
+
+}
---
+++ app/src/main/java/com/henriquealrs/android/semana3_aula3/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.henriquealrs.android.semana3_aula3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
+
+        if(viewModel.startTime == 0L) {
+            viewModel.startTime = SystemClock.elapsedRealtime()
+        }
+
+        chronometer.base = viewModel.startTime
+        chronometer.start()
+    }
+}
