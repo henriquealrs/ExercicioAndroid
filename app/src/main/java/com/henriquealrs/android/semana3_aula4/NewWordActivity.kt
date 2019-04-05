9e3ceebdf779f871acdac4a97a0e11a23598c08f
Henrique Silva <henriquealrs@gmail.com>
2019-04-05 11:09:17 -0300

    Semana 3 Aula 4

---
+++ app/src/main/java/com/henriquealrs/android/semana3_aula4/NewWordActivity.kt
@@ -0,0 +1,12 @@
+package com.henriquealrs.android.semana3_aula4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
